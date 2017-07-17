411bd4e8c8ee6fca2e435f715d92a19ce3d3ec0b
xingmengmeng <xingmengmeng@gomeholdings.com>
2017-07-17 17:35:00 +0800

    代码优化

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
-import Vuex from 'vuex';
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
-Vue.use(Vuex);
 
 import store from './store';/*vuex 状态引入*/
 
