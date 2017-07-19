097e99e0809b86633a674e02e1e7fb989497127a
xingmengmeng <xingmengmeng@gomeholdings.com>
2017-07-19 16:03:40 +0800

    新增公告保存成功后调用保存成功弹框效果

--- src/views/setting/notice.vue
+++ src/views/setting/notice.vue
@@ -3,9 +3,22 @@
         <h4>公告</h4>
         <textarea placeholder="在这里输入系统公告" v-model="noticeCon"></textarea>
         <input type="button" value="保存" class="keep" @click="saveNotice"/>
+        <transition name="slide-fade">
+            <success-box v-show="showSuccess"></success-box>
+        </transition>
     </div>
 </template>
 <style scoped lang="less">
+    .slide-fade-enter-active {
+        transition: all .2s ease;
+    }
+    .slide-fade-leave-active {
+        transition: all .4s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    .slide-fade-enter, .slide-fade-leave-to{
+        transform: translateX(100px);
+        opacity: 0;
+    }
     h4{
         padding: 10px 20px;
         margin-bottom: 15px;
@@ -40,18 +53,25 @@
     }
 </style>
 <script>
+import successBox from '../../components/successBox.vue';
 export default {
   data(){
       return{
         noticeCon:'',
+        showSuccess:false,
       }
+  },
+  components:{
+    'success-box':successBox,
   },
   methods:{
     saveNotice(){
         this.$http.post('/api/notice/save.gm',{"message":this.noticeCon},{emulateJSON:true}).then(function(res){
             if(res.data.code=='200'){
-                console.log('保存成功');
-                //this.$store.commit("changeNotice");
+                this.showSuccess=true;
+                setTimeout(()=>{
+                    this.showSuccess=false;
+                },2000);
                 this.$store.state.notice=this.noticeCon;
             }
         })
--- src/views/tagView/tagView.vue
+++ src/views/tagView/tagView.vue
@@ -138,16 +138,16 @@
     </section>
 </template>
 <style scoped lang="less">
-.slide-fade-enter-active {
-  transition: all .2s ease;
-}
-.slide-fade-leave-active {
-  transition: all .4s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-}
-.slide-fade-enter, .slide-fade-leave-to{
-  transform: translateX(100px);
-  opacity: 0;
-}
+    .slide-fade-enter-active {
+        transition: all .2s ease;
+    }
+    .slide-fade-leave-active {
+        transition: all .4s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    .slide-fade-enter, .slide-fade-leave-to{
+        transform: translateX(100px);
+        opacity: 0;
+    }
     .appList{
         height: 100%;
     }
