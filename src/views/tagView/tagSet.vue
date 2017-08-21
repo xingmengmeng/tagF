f0439c572383484a89c9f6a573f51a8756364f6f
xingmengmeng <xingmengmeng@gomeholdings.com>
2017-08-21 11:23:54 +0800

    完成拖拽组合测试

--- src/views/tagView/tagSet.vue
+++ src/views/tagView/tagSet.vue
@@ -1,12 +1,38 @@
 <template>
     <div>
-        <draggable v-model="tags" :move="getdata" @update="datadragEnd">
-            <transition-group>
-                <div v-for="element in tags" :key="element.id">
-                    {{element.name}}
+        <h1>Vue Dragable</h1>
+
+        <div class="drag">
+            <h2>List 1 v-dragable-for</h2>
+            <draggable id="list1" :list="list1" class="dragArea" :options="{group:{name:'people', put:false }}" @change="log">
+                <div v-for="(element, index) in list1"  :key="index">
+                    {{element.name}} {{index}}
                 </div>
-            </transition-group>
-    </draggable>
+             </draggable>
+             <h2>List 1 v-dragable-for</h2>
+             <draggable id="list1" :list="list3" class="dragArea" :options="{group:{name:'people', pull:'clone', put:false }}" @change="log">
+                <div v-for="(element, index) in list3"  :key="index">
+                    {{element.name}} {{index}}
+                </div>
+             </draggable>
+
+            <h2>List 2 v-dragable-for</h2>
+            <draggable id="list2" :list="list2" class="dragArea" :options="{group:'people'}" @change="log">
+                <div v-for="(element, index)  in list2"  :key="index">{{element.name}}</div>
+             </draggable>
+         </div>
+
+        <div class="normal">
+            <h2>List 1 v-for</h2>
+            <div>
+                <div v-for="(element, index) in list1" :key="index">{{element.name}}</div>
+             </div>
+
+            <h2>List 2 v-for</h2>
+            <div>
+                <div v-for="(element, index) in list2" :key="index">{{element.name}}</div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -14,24 +40,36 @@ import draggable from 'vuedraggable'
 export default {
     data(){
         return{
-            tags:[{
-                name:1,
-                id:1
-            }]
+            list1:[{name:"111", id:1}, 
+				{name:"111", id:2}, 
+				{name:"111", id:3},
+				{name:"111", id:4} ],
+            list2:[{name:"Juan", id:5}, 
+                {name:"Edgard", id:6}, 
+                {name:"Johnson", id:7} ],
+            list3:[{name:"222", id:8}, 
+                {name:"222", id:9}, 
+                {name:"222", id:10} ],
         }
-    },
+    },	
     components:{
         draggable
     },
-    methods:{
-        getdata (evt) {
-            console.log(evt.draggedContext.element.id)
-        },
-        datadragEnd (evt) {
-            console.log('拖动前的索引 :' + evt.oldIndex)
-            console.log('拖动后的索引 :' + evt.newIndex)
-            console.log(this.tags)
-        }
-    }
+	methods:{
+			add: function(){
+				this.list.push({name:'Juan'});
+			},
+			replace: function(){
+				this.list=[{name:'Edgard'}]
+			},
+			clone: function(el){
+				return {
+					name : el.name + ' cloned'
+				}
+			},
+			log: function (evt){
+				console.log(evt)
+			}
+		},
 }
 </script>
