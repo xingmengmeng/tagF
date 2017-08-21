47e111d36aeb619bcade6a989d24d7def24658a6
xingmengmeng <xingmengmeng@gomeholdings.com>
2017-08-21 11:32:24 +0800

    拖拽测试完成

--- src/views/tagView/tagSet.vue
+++ src/views/tagView/tagSet.vue
@@ -3,26 +3,26 @@
         <h1>Vue Dragable</h1>
 
         <div class="drag">
-            <h2>List 1 v-dragable-for</h2>
-            <draggable id="list1" :list="list1" class="dragArea" :options="{group:{name:'people', put:false }}" @change="log">
+            <h2>不拷贝拖拽</h2>
+            <draggable id="list1" :list="list1" class="dragArea" :options="{group:{name:'people', put:false }}">
                 <div v-for="(element, index) in list1"  :key="index">
                     {{element.name}} {{index}}
                 </div>
              </draggable>
-             <h2>List 1 v-dragable-for</h2>
-             <draggable id="list1" :list="list3" class="dragArea" :options="{group:{name:'people', pull:'clone', put:false }}" @change="log">
+             <h2>拷贝拖拽</h2>
+             <draggable id="list3" :list="list3" class="dragArea" :options="{group:{name:'people', pull:'clone', put:false }}">
                 <div v-for="(element, index) in list3"  :key="index">
                     {{element.name}} {{index}}
                 </div>
              </draggable>
 
-            <h2>List 2 v-dragable-for</h2>
+            <h2>目标位置</h2>
             <draggable id="list2" :list="list2" class="dragArea" :options="{group:'people'}" @change="log">
                 <div v-for="(element, index)  in list2"  :key="index">{{element.name}}</div>
              </draggable>
          </div>
 
-        <div class="normal">
+        <!--<div class="normal">
             <h2>List 1 v-for</h2>
             <div>
                 <div v-for="(element, index) in list1" :key="index">{{element.name}}</div>
@@ -32,7 +32,7 @@
             <div>
                 <div v-for="(element, index) in list2" :key="index">{{element.name}}</div>
             </div>
-        </div>
+        </div>-->
     </div>
 </template>
 <script>
@@ -68,7 +68,7 @@ export default {
 				}
 			},
 			log: function (evt){
-				console.log(evt)
+				console.log(JSON.stringify(this.list2));
 			}
 		},
 }
